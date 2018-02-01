9920357c942f61f46556af48ffb66fc893c24ff3
Alex Church <35304852+alexchurch29@users.noreply.github.com>
2018-02-01 17:50:11 -0500

    Add files via upload
    updated skater query to include G, A1, A2, A, P, P1

--- nhl_db.py
+++ nhl_db.py
@@ -22,22 +22,65 @@ def update():
     z.to_sql('schedule', conn, if_exists='replace')
 
 # creates full list of skaters
-# NAME, SEASON**, TEAM*, POS, GP, TOI
+# NAME, SEASON**, TEAM*, POS, GP, TOI, G
 # to aggregate for multi player teams add r.team to GROUP BY
 # to aggregate for multiple seasons remove z.season from GROUP BY
-skaters = pd.read_sql_query ('''
-SELECT r.name, z.season, r.team, r.pos, COUNT(r.name) as GP, s.TOI
+skaters_all_strengths = pd.read_sql_query ('''
+SELECT r.name, z.season, r.team, r.pos, COUNT(r.name) as GP, icetime.TOI, goal.G, assist1.A1, assist2.A2, 
+    (assist1.A1 + assist2.A2) as A,  (goal.G + (assist1.A1 + assist2.A2)) as P, (goal.G + assist1.A1) as P1
+
 FROM rosters r 
+
 INNER JOIN schedule z 
 ON r.game_id = z.game_id
+
 INNER JOIN (SELECT player, ROUND(SUM(duration)/60,2) as TOI
     FROM shifts
-    GROUP BY player) as s
-ON s.player = r.name
+    GROUP BY player) as icetime
+ON icetime.player = r.name
+
+LEFT OUTER JOIN (SELECT name, count(name) as G
+    FROM (SELECT r.name, p.p1_team, p.p1_num
+    FROM pbp p
+    INNER JOIN rosters r
+    ON (r.game_id = p.game_id
+        and r.team=p.p1_team
+        and r.num=p.p1_num)
+    WHERE p.event_type = 'GOAL'
+        and p.period != 5)
+    GROUP BY name) as goal
+ON goal.name = r.name
+
+LEFT OUTER JOIN (SELECT name, count(name) as A1
+    FROM (SELECT r.name, p.p2_team, p.p2_num
+    FROM pbp p
+    INNER JOIN rosters r
+    ON (r.game_id = p.game_id
+        and r.team=p.p2_team
+        and r.num=p.p2_num)
+    WHERE p.event_type = 'GOAL'
+        and p.period != 5)
+    GROUP BY name) as assist1
+ON assist1.name = r.name
+
+LEFT OUTER JOIN (SELECT name, count(name) as A2
+    FROM (SELECT r.name, p.p3_team, p.p3_num
+    FROM pbp p
+    INNER JOIN rosters r
+    ON (r.game_id = p.game_id
+        and r.team=p.p3_team
+        and r.num=p.p3_num)
+    WHERE p.event_type = 'GOAL'
+        and p.period != 5)
+    GROUP BY name) as assist2
+ON assist2.name = r.name
+    
 WHERE r.pos != 'G' 
     AND r.scratch is null
+    
 GROUP BY z.season, r.name, r.pos
-ORDER BY r.name, z.season;''', conn)
+
+ORDER BY P DESC, r.name;''', conn)
 
 
 
